6b2200be5955a710c9d5ef6726f8b32c8c69457f
nuyoi <nuyoi@naver.com>
2020-11-12 23:24:36 +0900

    아코디언 셀렉트 컴포넌트

---
+++ src/components/AccordionSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>
+      <a
+        class="p-4 block bg-gray-600 hover:bg-gray-800 no-underline text-white border-b-2 border-white flex justify-between"
+        @click.prevent="active = !active"
+      >
+        <span>{{ title }}</span>
+        <span v-show="!active">&#9660;</span>
+        <span v-show="active">&#9650;</span>
+      </a>
+    </div>
+    <div v-show="active">
+      <a
+        v-for="item in items"
+        :key="item.value"
+        class="px-4 py-2 block"
+        @click="selected(item)"
+      >
+        {{ item.show }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    items: Array
+  },
+  data() {
+    return {
+      active: false
+    };
+  },
+  methods: {
+    selected(item) {
+      this.active = false;
+      this.$emit("clicked", item);
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-screen bg-gray-400">
+  <div class="w-screen">
     <accordion title="title">
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate
@@ -8,14 +8,41 @@
         consequuntur voluptatibus! Asperiores saepe doloremque cum minima.
       </p>
     </accordion>
+    <accordion-select
+      :title="period.show"
+      :items="[
+        { show: '3주 완성 프로젝트', value: 3 },
+        { show: '4주 완성 프로젝트', value: 4 },
+        { show: '5주 완성 프로젝트', value: 5 },
+        { show: '6주 완성 프로젝트', value: 6 },
+        { show: '직접입력', value: 7 }
+      ]"
+      @clicked="selectedPeriod"
+    >
+    </accordion-select>
+    <div v-show="period.value === 7">
+      <input type="text" v-model="period.value" />주 완성 프로젝트
+    </div>
   </div>
 </template>
 
 <script>
 import Accordion from "@/components/Accordion";
+import AccordionSelect from "@/components/AccordionSelect";
 export default {
   components: {
-    Accordion
+    Accordion,
+    AccordionSelect
+  },
+  data() {
+    return {
+      period: { show: "기간 선택", value: null }
+    };
+  },
+  methods: {
+    selectedPeriod(item) {
+      this.period = item;
+    }
   }
 };
 </script>
