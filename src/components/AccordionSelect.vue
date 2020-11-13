91cc43990066e3b94288d9e0cb2b83f992f0c760
nuyoi <nuyoi@naver.com>
2020-11-13 17:53:41 +0900

    아코디언 셀렉트 컴포넌트

--- src/components/AccordionSelect.vue
+++ src/components/AccordionSelect.vue
@@ -2,19 +2,19 @@
   <div>
     <div>
       <a
-        class="p-4 block bg-gray-600 hover:bg-gray-800 no-underline text-white border-b-2 border-white flex justify-between"
+        class="flex justify-between p-2 no-underline cursor-pointer"
         @click.prevent="active = !active"
       >
         <span>{{ title }}</span>
-        <span v-show="!active">&#9660;</span>
-        <span v-show="active">&#9650;</span>
+        <span v-show="!active"><i class="fas fa-chevron-down"></i></span>
+        <span v-show="active"><i class="fas fa-chevron-up"></i></span>
       </a>
     </div>
     <div v-show="active">
       <a
         v-for="item in items"
         :key="item.value"
-        class="px-4 py-2 block"
+        class="block px-4 py-2 cursor-pointer"
         @click="selected(item)"
       >
         {{ item.text }}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="w-screen">
-    <accordion-select
-      :title="period.text"
-      :items="[
-        { text: '3주 완성 프로젝트', value: 3 },
-        { text: '4주 완성 프로젝트', value: 4 },
-        { text: '5주 완성 프로젝트', value: 5 },
-        { text: '6주 완성 프로젝트', value: 6 },
-        { text: '직접 입력', value: '' }
-      ]"
-      @clicked="selectedPeriod"
-    >
-    </accordion-select>
-    <div v-show="period.text === '직접 입력'">
-      <input type="number" v-model.number="period.value" />
-      <span>
-        주 완성 프로젝트
-      </span>
+    <div class="flex">
+      <div class="w-1/3 p-4">기간</div>
+      <div class="w-2/3 px-4 py-2">
+        <accordion-select
+          :title="period.text"
+          :items="[
+            { text: '3주 완성 프로젝트', value: 3 },
+            { text: '4주 완성 프로젝트', value: 4 },
+            { text: '5주 완성 프로젝트', value: 5 },
+            { text: '6주 완성 프로젝트', value: 6 },
+            { text: '직접 입력', value: '' }
+          ]"
+          @clicked="selectedPeriod"
+        >
+        </accordion-select>
+        <div v-show="period.text === '직접 입력'" class="flex p-2">
+          <input
+            type="number"
+            v-model="tempPeriod"
+            class="w-1/6 mr-2 border border-gray-400 rounded-md"
+          />
+          <span class="w-3/6">
+            주 완성 프로젝트
+          </span>
+          <button class="w-2/6" @click.prevent="typedPeriod">
+            완료
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,12 +41,41 @@ export default {
   },
   data() {
     return {
-      period: { text: "기간 선택", value: "" }
+      period: { text: "기간 선택", value: "" },
+      tempPeriod: ""
     };
   },
   methods: {
     selectedPeriod(item) {
       this.period = item;
+
+      //하루동안의 운동 1개, 식단 1개 오브젝트 초기화
+      let day = {
+        train: this.workoutlist,
+        diet: this.foodlist
+      };
+
+      let week = Array.from({ length: this.count }, () => day);
+      let period = 0;
+      period = this.period.value;
+      //프로젝트 기간으로 커리큘럼 셋팅, period는 주(week)를 의미
+      this.curriculum = [];
+      for (let i = 0; i < period; i++) {
+        this.curriculum.push(week);
+      }
+    },
+    typedPeriod() {
+      let day = {
+        train: this.workoutlist,
+        diet: this.foodlist
+      };
+      let week = Array.from({ length: this.count }, () => day);
+      let period = 0;
+      period = this.tempPeriod;
+      this.curriculum = [];
+      for (let i = 0; i < period; i++) {
+        this.curriculum.push(week);
+      }
     }
   }
 };
