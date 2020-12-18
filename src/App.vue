20d1eb5ead497a5033049dc6b1f7548bd04d0ac5
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-12-18 11:48:22 +0900

    이씨

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="app" class="flex h-screen w-full flex-row">
-    <nav class="block fixed z-10 top-0 border-b inset-x-0 uppercase px-10 py-3">
+  <div id="app">
+    <div id="nav" class="px-10 py-3 uppercase border-b">
       <div class="hidden">
         <router-link to="/header" class="bg-gray-400">header</router-link>
       </div>
       <router-link to="/nav" class="text-lg">nav</router-link>
-      <router-link to="/" class="text-lg mx-10">home</router-link>
-      <router-link to="/slider" class="text-lg mr-10">slider</router-link>
-      <router-link to="/chart" class="text-lg mr-10">chart</router-link>
-      <router-link to="/crud" class="text-lg mr-10">crud</router-link>
-      <router-link to="/detail" class="text-lg mr-10">detail</router-link>
+      <router-link to="/" class="mx-10 text-lg">home</router-link>
+      <router-link to="/slider" class="mr-10 text-lg">slider</router-link>
+      <router-link to="/chart" class="mr-10 text-lg">chart</router-link>
+      <router-link to="/crud" class="mr-10 text-lg">crud</router-link>
+      <router-link to="/detail" class="mr-10 text-lg">detail</router-link>
       <router-link to="/black" class="text-lg">black</router-link>
-    </nav>
-    <div class="flex justify-center mt-16">
+    </div>
+    <div class="flex justify-center">
       <router-view class="min-w-400 max-w-1190" />
     </div>
   </div>
--- src/views/Black.vue
+++ src/views/Black.vue
@@ -1,27 +1,39 @@
 <template>
-  <div>
-    <div
-      class="inline-block pb-5 overflow-x-auto scrolling-touch whitespace-nowrap disable-scrollbars"
-    >
+  <div class="w-64 py-5">
+    <div class="w-64">
+      <div class="inline-block pb-5 overflow-x-hidden whitespace-no-wrap">
+        <button
+          v-for="(item, i) in data"
+          :key="i"
+          type="button"
+          class="w-32 h-12 mr-3 text-lg font-bold text-center bg-white rounded-full shadow md:text-xl text-secondly-400"
+        >
+          운동{{ i + 1 }}
+        </button>
+        <button
+          @click="addData"
+          type="button"
+          class="w-20 h-12 mr-3 text-lg font-bold text-center bg-white rounded-full shadow md:text-xl text-secondly-400"
+        >
+          +
+        </button>
+      </div>
       <button
-        v-for="(item, i) in data"
-        :key="i"
-        type="button"
-        class="w-32 h-12 mr-3 text-lg font-bold text-center bg-white rounded-full shadow md:text-xl text-secondly-400"
+        @click="minusData"
+        class="block mt-4 font-semibold text-basicOrange"
       >
-        운동{{ i + 1 }}
-      </button>
-      <button
-        @click="addData"
-        type="button"
-        class="w-20 h-12 mr-3 text-lg font-bold text-center bg-white rounded-full shadow md:text-xl text-secondly-400"
-      >
-        +
+        삭제
       </button>
     </div>
-    <button @click="minusData" class="block text-basicOrange font-semibold">
-      삭제
-    </button>
+    <!-- <div class="pub__scroll-indicator">
+      <span class="pub__scroll-indicator__bar-wrapper">
+        <span
+          class="pub__scroll-indicator__bar"
+          style="width: 29.3633%; transform: translateX(240.562%);"
+        >
+        </span>
+      </span>
+    </div> -->
   </div>
 </template>
 
@@ -43,4 +55,77 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+/* .pub__scroll-indicator {
+  height: 1.5rem;
+  position: relative;
+  width: 100%;
+  outline: none;
+  border: 0;
+  padding: 0;
+  background: transparent;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.pub__scroll-indicator__bar-wrapper {
+  width: 100%;
+  background: #dfdfdf;
+  -webkit-transform: translateX(0);
+  transform: translateX(0);
+  height: 0.125rem;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  display: block;
+  border-radius: 4px;
+}
+
+.pub__scroll-indicator__bar {
+  will-change: transform;
+  background: #111;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.125rem;
+  -webkit-transform-origin: 0 0;
+  transform-origin: 0 0;
+  display: block;
+  border-radius: 4px;
+  cursor: -webkit-grab;
+  cursor: grab;
+}
+
+.pub__scroll-indicator--dragging .pub__scroll-indicator__bar-wrapper,
+.pub__scroll-indicator--dragging .pub__scroll-indicator__bar,
+.pub__scroll-indicator:hover .pub__scroll-indicator__bar-wrapper,
+.pub__scroll-indicator:hover .pub__scroll-indicator__bar {
+  height: 0.25rem;
+}
+.pub__scroll-indicator--dragging .pub__scroll-indicator__bar,
+.pub__scroll-indicator--dragging .pub__scroll-indicator__bar:hover {
+  height: 0.5rem;
+  top: -0.125rem;
+}
+.pub__scroll-indicator--dragging .pub__scroll-indicator__bar-wrapper,
+.pub__scroll-indicator--dragging .pub__scroll-indicator__bar,
+.pub__scroll-indicator:hover .pub__scroll-indicator__bar-wrapper,
+.pub__scroll-indicator:hover .pub__scroll-indicator__bar {
+  height: 0.25rem;
+}
+.pub__scroll-indicator__bar-dragging {
+  cursor: -webkit-grabbing;
+  cursor: grabbing;
+}
+
+.pub__carousel__content {
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  -webkit-overflow-scrolling: touch;
+  position: relative;
+  padding-top: 0.25rem;
+  padding-bottom: 1.5rem;
+} */
+</style>
